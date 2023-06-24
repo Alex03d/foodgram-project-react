dc133b0da19754123f6811d70988252033721599
Alexander Dondokov <adondokov@gmail.com>
2023-06-24 12:34:49 +0800

    в целом все json отрабатывают / но фотмат ответов не везде совпадает с ТЗ / написал простые тесты

--- backend/recipes/test_recipes.py
+++ backend/recipes/test_recipes.py
@@ -1,8 +1,8 @@
 import pytest
 from rest_framework.test import APIClient
 from users.models import User
-from recipes.models import Recipe, Tag, Ingredient, RecipeIngredient
-from api.serializers import RecipeSerializer
+from recipes.models import Recipe, Tag, Ingredient, RecipeIngredient, Subscription
+# from api.serializers import RecipeSerializer
 
 
 @pytest.fixture
@@ -47,6 +47,24 @@ def test_ingredient(db):
         name='Test ingredient',
         measurement_unit='g'
     )
+
+
+@pytest.fixture
+def test_ingredients(db):
+    return [
+        Ingredient.objects.create(
+            name='Test ingredient 1',
+            measurement_unit='g'
+        ),
+        Ingredient.objects.create(
+            name='Test ingredient 2',
+            measurement_unit='kg'
+        ),
+        Ingredient.objects.create(
+            name='Another ingredient',
+            measurement_unit='ml'
+        ),
+    ]
 
 
 @pytest.fixture
@@ -120,3 +138,61 @@ def test_download_shopping_cart(db, test_user, test_recipe, api_client):
 
     # Проверяем содержимое файла, которое может зависеть от вашей реализации
     assert 'Test ingredient - 10 g' in str(response.content), 'The ingredient list is incorrect'
+
+
+def test_shopping_list(db, test_user, test_recipe, api_client):
+    api_client.force_authenticate(user=test_user)
+    api_client.post(f'/api/recipes/{test_recipe.id}/shopping_cart/')
+    response = api_client.get('/api/recipes/download_shopping_cart/')
+    assert response.status_code == 200
+
+
+def test_get_ingredients(db, test_ingredients, api_client):
+    response = api_client.get('/api/ingredients/')
+    assert response.status_code == 200
+
+
+def test_get_specific_ingredient(db, test_ingredients, api_client):
+    ingredient_id = test_ingredients[0].id  # Получаем id первого ингредиента из списка
+    response = api_client.get(f'/api/ingredients/{ingredient_id}/')
+    assert response.status_code == 200
+    assert response.data['id'] == ingredient_id  # Проверяем, что в ответе получен нужный ингредиент
+
+
+def test_get_tags(db, test_tags, api_client):
+    response = api_client.get('/api/tags/')
+    assert response.status_code == 200
+    assert all(tag.name in [data['name'] for data in response.data['results']] for tag in test_tags)
+
+
+@pytest.fixture
+def test_subscription(db, test_user, create_user):
+    another_user = create_user(email='another_user@test.com', password='test_password')
+    return Subscription.objects.create(user=test_user, author=another_user)
+
+
+def test_subscribe(db, test_user, test_subscription, api_client):
+    api_client.force_authenticate(user=test_user)
+    response = api_client.post(f'/api/users/{test_subscription.author.id}/subscribe/')
+    assert response.status_code == 400  # because already subscribed
+
+
+def test_unsubscribe(db, test_user, test_subscription, api_client):
+    api_client.force_authenticate(user=test_user)
+    response = api_client.delete(f'/api/users/{test_subscription.author.id}/subscribe/')
+    assert response.status_code == 204
+
+
+def test_subscriptions(db, test_user, test_subscription, api_client):
+    api_client.force_authenticate(user=test_user)
+    response = api_client.get('/api/users/subscriptions/')
+    assert response.status_code == 200
+    assert len(response.data['results']) == 1
+
+
+@pytest.fixture
+def test_tags(db):
+    return [
+        Tag.objects.create(name='Test tag 1', color='123456', slug='test1'),
+        Tag.objects.create(name='Test tag 2', color='654321', slug='test2'),
+    ]
