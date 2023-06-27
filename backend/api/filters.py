af22608bc6da63e900b3daf43dd27143e305cd18
Alexander Dondokov <adondokov@gmail.com>
2023-06-27 13:55:28 +0800

    поправил ПЕП8 для фильтров

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -26,13 +26,7 @@ class RecipeFilter(django_filters.FilterSet):
             return queryset.filter(in_shopping_cart__user=self.request.user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(in_shopping_cart__user=self.request.user)
-        return queryset
-
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
             return queryset.filter(favorited_by__user=self.request.user)
         return queryset
-
